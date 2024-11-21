3c5f724e45ebd4d1ce183e082b3257e59bbbbf70
Raj Rajhans <me@rajrajhans.com>
2024-11-22 00:11:52 +0530

    chore: update log

--- server.js
+++ server.js
@@ -3,21 +3,26 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  console.log('🔍 Request received:', {
-    timestamp: new Date().toISOString(),
-    ip: req.ip,
-    method: req.method,
-    path: req.path,
-    headers: {
-      host: req.headers.host,
-      'user-agent': req.headers['user-agent'],
-      accept: req.headers.accept,
-    },
-    query: req.query,
-    protocol: req.protocol,
-    hostname: req.hostname,
-    userAgent: req.get('user-agent'),
-  });
+  console.log(
+    JSON.stringify({
+      message: '🔍 Request received',
+      timestamp: new Date().toISOString(),
+      ip: req.ip,
+      realIp:
+        req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.ip,
+      method: req.method,
+      path: req.path,
+      headers: {
+        host: req.headers.host,
+        'user-agent': req.headers['user-agent'],
+        accept: req.headers.accept,
+      },
+      query: req.query,
+      protocol: req.protocol,
+      hostname: req.hostname,
+      userAgent: req.get('user-agent'),
+    })
+  );
 
   res.send('OK');
 });
